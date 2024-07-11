32380526e6e7080d807f2d5089bfbebb64f35c11
ansynneuhaus@gmail.com <ansynneuhaus@gmail.com>
2024-07-11 15:40:31 -0700

    added third person view cam

--- Assets/Scripts/Util/CameraFollow.cs
+++ Assets/Scripts/Util/CameraFollow.cs
@@ -2,40 +2,22 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    [Header("References")]
+    public UnityEngine.Transform orientation;
+    public UnityEngine.Transform player;
+    public UnityEngine.Transform playerObj;
 
-    private const float YMin = -50.0f;
-    private const float YMax = 50.0f;
+    public Rigidbody rb;
 
-    public Transform lookAt;
+    public float rotationSpeed;
 
-    public Transform Player;
-
-    public float distance = 10.0f;
-    private float currentX = 0.0f;
-    private float currentY = 0.0f;
-    public float sensivity = 4.0f;
-
-
-    // Start is called before the first frame update
-    void Start()
+    void Update()
     {
+        UnityEngine.Vector3 viewDir = player.position - new UnityEngine.Vector3(transform.position.x, transform.position.y, transform.position.z);
+        orientation.forward = viewDir.normalized;
+
 
     }
 
-    // Update is called once per frame
-    void LateUpdate()
-    {
 
-        currentX += Input.GetAxis("Mouse X") * sensivity * Time.deltaTime;
-        currentY += Input.GetAxis("Mouse Y") * sensivity * Time.deltaTime;
-
-        currentY = Mathf.Clamp(currentY, YMin, YMax);
-
-        Vector3 Direction = new Vector3(0, 0, -distance);
-        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
-        transform.position = lookAt.position + rotation * Direction;
-
-        transform.LookAt(lookAt.position);
-
-    }
 }
